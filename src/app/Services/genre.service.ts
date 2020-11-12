5a6a12d224897213ef634e9d632239bb7b723276
JenniferGome <jennifergomezdiaz1104@gmail.com>
2020-11-12 16:46:00 -0600

    se creo nuevo repositorio

---
+++ src/app/Services/genre.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Genre } from '../Models/Genre';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenreService {
+
+  apiURL: String = 'https://key-code-b.herokuapp.com';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  createGenre(formData){
+    return this.http.post<Genre>(`${this.apiURL}/genre/create`, formData)
+  }
+
+  getAll(){
+    return this.http.get(`${this.apiURL}/genre/getAll`)
+  }
+
+
+}
